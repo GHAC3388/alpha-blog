ee2dfb3835e4af32333857ca4c966f68490d596f
Rails Udemy Course <user@email.com>
2018-01-25 00:11:46 +0000

    Add partials and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
     
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to article_path(@article)
+      redirect_to articles_path(@article)
     else
       render 'new'
     end
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
   
     @article = Article.find(params[:id])
   
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
   def index
